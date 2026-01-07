ddf74e52658ec9e4af997252db19eddab26ec2ad
Anil Kumal <anilkumal557@gmail.com>
2026-01-07 08:56:51 +0545

    user auth

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import config from "./config/config.js";
-
+import cookieParser from "cookie-parser";
 
 import connectDB from "./config/database.js";
 import studentRoute from "./routes/student.route.js";
+import userRoute from "./routes/users.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
@@ -15,10 +17,12 @@ app.get("/", (req, res) => {
   });
 });
 
-
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/", studentRoute);
+app.use("/api/users", userRoute); 
+app.use("/api/auth", authRoutes);
 
 app.listen(config.port,() => {
   console.log(`Server running at port: ${config.port}`);
---
+++ backend/src/services/auth.service.js
@@ -0,0 +1,25 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email });
+  if (!user) throw new Error("Invalid email or password");
+
+  // Compare entered password with hashed password in DB
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) throw new Error("Invalid email or password");
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return {
+    message: "Successfully logged in",
+    _id: user._id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+    token,
+  };
+};
